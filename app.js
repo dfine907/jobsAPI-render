8d6df5e9f2d49aa3981cf84e81b192fe7d96be78
Dina <dinagiacobbi@gmail.com>
2024-11-08 10:42:27 -0900

    Edits to app.js route

--- app.js
+++ app.js
@@ -44,6 +44,10 @@ app.use('/api/v1/jobs', authenticateUser,  jobsRouter)
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
+app.get('/', (req, res) => {
+  res.send('Jobs API')
+}) 
+
 const port = process.env.PORT || 3000
 
 const start = async () => {
